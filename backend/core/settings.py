467f1f89b2a04cfdb1e97378712e06bea7556b77
Karl Haines <kmhnashville@gmail.com>
2026-03-19 22:30:01 -0500

    Added .env*.example files for backend and frontend, also made some changes in backend/core/settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "decure-z6dh*i8cjajq$o6lg-@$%3v06vpl!irr9+v0=+d&5d$f#-(&#t"
+SECRET_KEY = "use-env-to-change-me"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
