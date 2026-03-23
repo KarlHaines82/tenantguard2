85e848a1b4bd2e9161c0bf9443fca0858e428c59
Karl Haines <kmhnashville@gmail.com>
2026-03-23 16:23:33 -0500

    Added google analytics code to frontend

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,6 +3,16 @@ import {SessionProvider} from "next-auth/react";
 import "../styles/globals.css";
 import Chat from "@/components/Chat";
 import StaffTodoWidget from "@/components/StaffTodoWidget";
+import { GoogleAnalytics } from '@next/third-parties/google'
+ 
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-VSHHRG05DN" />
+    </>
+  )
+}
 
 export default function App({Component, pageProps: {session, ...pageProps}}: AppProps) {
   return (
