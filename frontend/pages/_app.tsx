a082658d3b68ae822dbdc831428283cce8972e7c
Karl Haines <kmhnashville@gmail.com>
2026-03-23 17:09:34 -0500

    edited pages/_app.tsx to add google anal

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,21 +4,13 @@ import "../styles/globals.css";
 import Chat from "@/components/Chat";
 import StaffTodoWidget from "@/components/StaffTodoWidget";
 import { GoogleAnalytics } from '@next/third-parties/google'
- 
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <GoogleAnalytics gaId="G-VSHHRG05DN" />
-    </>
-  )
-}
 
 export default function App({Component, pageProps: {session, ...pageProps}}: AppProps) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
       <Chat />
+      <GoogleAnalytics gaId="G-VSHHRG05DN" />
       <StaffTodoWidget />
     </SessionProvider>
   );
